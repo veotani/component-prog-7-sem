1b927c4c4798cc0a59b35e231d8551374ec97fd8
Антон Орлов Сергеевич <ion6431@gmail.com>
2017-12-14 02:09:47 +0300

    Создание графиков

---
+++ lab9/lab9/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.графикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.точкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.гистограммаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.диапозонToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дваГрафикаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.col2,
+            this.col3,
+            this.col4,
+            this.col5,
+            this.col6});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(648, 97);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // col1
+            // 
+            this.col1.HeaderText = "2005";
+            this.col1.Name = "col1";
+            // 
+            // col2
+            // 
+            this.col2.HeaderText = "2006";
+            this.col2.Name = "col2";
+            // 
+            // col3
+            // 
+            this.col3.HeaderText = "2007";
+            this.col3.Name = "col3";
+            // 
+            // col4
+            // 
+            this.col4.HeaderText = "2008";
+            this.col4.Name = "col4";
+            // 
+            // col5
+            // 
+            this.col5.HeaderText = "2009";
+            this.col5.Name = "col5";
+            // 
+            // col6
+            // 
+            this.col6.HeaderText = "2010";
+            this.col6.Name = "col6";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.IsEnableHZoom = false;
+            this.zedGraphControl1.IsEnableVZoom = false;
+            this.zedGraphControl1.IsEnableWheelZoom = false;
+            this.zedGraphControl1.Location = new System.Drawing.Point(0, 130);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(822, 402);
+            this.zedGraphControl1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.графикToolStripMenuItem,
+            this.точкиToolStripMenuItem,
+            this.гистограммаToolStripMenuItem,
+            this.диапозонToolStripMenuItem,
+            this.дваГрафикаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(834, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // графикToolStripMenuItem
+            // 
+            this.графикToolStripMenuItem.Name = "графикToolStripMenuItem";
+            this.графикToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.графикToolStripMenuItem.Text = "График";
+            this.графикToolStripMenuItem.Click += new System.EventHandler(this.графикToolStripMenuItem_Click);
+            // 
+            // точкиToolStripMenuItem
+            // 
+            this.точкиToolStripMenuItem.Name = "точкиToolStripMenuItem";
+            this.точкиToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.точкиToolStripMenuItem.Text = "Точки";
+            this.точкиToolStripMenuItem.Click += new System.EventHandler(this.точкиToolStripMenuItem_Click);
+            // 
+            // гистограммаToolStripMenuItem
+            // 
+            this.гистограммаToolStripMenuItem.Name = "гистограммаToolStripMenuItem";
+            this.гистограммаToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.гистограммаToolStripMenuItem.Text = "Гистограмма";
+            this.гистограммаToolStripMenuItem.Click += new System.EventHandler(this.гистограммаToolStripMenuItem_Click);
+            // 
+            // диапозонToolStripMenuItem
+            // 
+            this.диапозонToolStripMenuItem.Name = "диапозонToolStripMenuItem";
+            this.диапозонToolStripMenuItem.Size = new System.Drawing.Size(117, 20);
+            this.диапозонToolStripMenuItem.Text = "Две гистограммы";
+            this.диапозонToolStripMenuItem.Click += new System.EventHandler(this.диапозонToolStripMenuItem_Click);
+            // 
+            // дваГрафикаToolStripMenuItem
+            // 
+            this.дваГрафикаToolStripMenuItem.Name = "дваГрафикаToolStripMenuItem";
+            this.дваГрафикаToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.дваГрафикаToolStripMenuItem.Text = "Два графика";
+            this.дваГрафикаToolStripMenuItem.Click += new System.EventHandler(this.дваГрафикаToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(655, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 97);
+            this.button1.TabIndex = 3;
+            this.button1.Tag = "button1";
+            this.button1.Text = "COLOR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(656, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 44);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Color 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(656, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 47);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Color 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 544);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem графикToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem точкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem гистограммаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem диапозонToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дваГрафикаToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab9/lab9/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace lab9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.RowCount = 2;
+            dataGridView1.Rows[0].HeaderCell.Value = "Математика";
+            dataGridView1.Rows[1].HeaderCell.Value = "Русский язык";
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            var rnd = new Random();
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                int mathMark = rnd.Next(35, 96);
+                dataGridView1.Rows[0].Cells[i].Value = mathMark;
+                int rusMark = rnd.Next(35, 96);
+                dataGridView1.Rows[1].Cells[i].Value = rusMark;
+            }
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void графикToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+            button1.Show();
+            zedGraphControl1.GraphPane.XAxis.MajorGrid.IsVisible = true;
+            zedGraphControl1.GraphPane.YAxis.MajorGrid.IsZeroLine = true;
+            zedGraphControl1.GraphPane.Title.Text = графикToolStripMenuItem.Text;
+            
+
+            PointPairList points = new PointPairList();
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                points.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value),
+                    (Convert.ToInt64(value: dataGridView1.Rows[0].Cells[i].Value) +
+                     Convert.ToInt64(dataGridView1.Rows[1].Cells[i].Value)) / 2);
+            }
+            var minY = points.Min(m => m.Y);
+            var min = points.First(m => m.Y == minY);
+            var maxY = points.Max(m => m.Y);
+            var max = points.First(m => m.Y == maxY);
+
+            //ArrowObj arrowMin = new ArrowObj(min.X+0.1, min.Y - 5, min.X, min.Y - 0.05);
+            //ArrowObj arrowMax = new ArrowObj(max.X + 0.1, max.Y + 5, max.X, max.Y + 0.05);
+            TextObj text1 = new TextObj("Min", min.X, min.Y - 3);
+            TextObj text2 = new TextObj("Max", max.X, max.Y + 3);
+
+            text1.FontSpec.Border.IsVisible = false;
+            text2.FontSpec.Border.IsVisible = false;
+            text2.FontSpec.FontColor = Color.Blue;
+            pane.GraphObjList.Add(text1);
+            pane.GraphObjList.Add(text2);
+            //pane.GraphObjList.Add(arrowMin);
+            //pane.GraphObjList.Add(arrowMax);
+            pane.XAxis.Title.Text = "Год";
+            pane.YAxis.Title.Text = "Средний балл";
+            var line = pane.AddCurve("Средний балл", points, Color.Blue, SymbolType.None);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void точкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+            button1.Show();
+            zedGraphControl1.GraphPane.Title.Text = точкиToolStripMenuItem.Text;
+            zedGraphControl1.GraphPane.XAxis.MajorGrid.IsVisible = true;
+            zedGraphControl1.GraphPane.YAxis.MajorGrid.IsZeroLine = true;
+            PointPairList points = new PointPairList();
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                points.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value),
+                    (Convert.ToInt64(value: dataGridView1.Rows[0].Cells[i].Value) +
+                     Convert.ToInt64(dataGridView1.Rows[1].Cells[i].Value)) / 2);
+            }
+            pane.XAxis.Title.Text = "Год";
+            pane.YAxis.Title.Text = "Средний балл";
+            var line = pane.AddCurve("Средний балл", points, Color.Blue, SymbolType.Circle);
+            line.Line.IsVisible = false;
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            //PointPairList mathPoints = new PointPairList();
+            //PointPairList rusPoints = new PointPairList(); 
+            //GraphPane pane = zedGraphControl1.GraphPane;
+            //pane.CurveList.Clear();
+            //for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            //{
+            //    rusPoints.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value), Convert.ToInt64(dataGridView1.Rows[1].Cells[i].Value));
+            //    mathPoints.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value), Convert.ToInt64(dataGridView1.Rows[0].Cells[i].Value));
+            //}
+            //pane.XAxis.Title.Text = "Год";
+            //pane.YAxis.Title.Text = "Средний балл";
+            //var line1 = pane.AddCurve("Русский язык", rusPoints, Color.Blue, SymbolType.Circle);
+            //var line2 = pane.AddCurve("Математика", mathPoints, Color.BlueViolet, SymbolType.Circle);
+            //line1.Line.IsVisible = false;
+            //line2.Line.IsVisible = false;
+            //zedGraphControl1.AxisChange();
+            //zedGraphControl1.Invalidate();
+        }
+
+        private void гистограммаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button2.Hide();
+            button3.Hide();
+            button1.Show();
+            zedGraphControl1.GraphPane.Title.Text = гистограммаToolStripMenuItem.Text;
+            PointPairList points = new PointPairList();
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                points.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value),
+                    (Convert.ToInt64(value: dataGridView1.Rows[0].Cells[i].Value) +
+                     Convert.ToInt64(dataGridView1.Rows[1].Cells[i].Value)) / 2);
+            }
+            pane.XAxis.Title.Text = "Год";
+            pane.YAxis.Title.Text = "Средний балл";
+            pane.AddBar("Средний балл", points, Color.Blue);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+
+        }
+
+        private void дваГрафикаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1.Hide();
+            button2.Show();
+            button3.Show();
+            zedGraphControl1.GraphPane.Title.Text = дваГрафикаToolStripMenuItem.Text;
+            PointPairList mathPoints = new PointPairList();
+            PointPairList rusPoints = new PointPairList();
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                rusPoints.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value), Convert.ToInt64(dataGridView1.Rows[1].Cells[i].Value));
+                mathPoints.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value), Convert.ToInt64(dataGridView1.Rows[0].Cells[i].Value));
+            }
+            pane.XAxis.Title.Text = "Год";
+            pane.YAxis.Title.Text = "Средний балл";
+            pane.AddCurve("Русский язык", rusPoints, Color.Blue, SymbolType.None);
+            pane.AddCurve("Математика", mathPoints, Color.BlueViolet, SymbolType.None);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void диапозонToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1.Hide();
+            button2.Show();
+            button3.Show();
+            PointPairList mathPoints = new PointPairList();
+            PointPairList rusPoints = new PointPairList();
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                rusPoints.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value), Convert.ToInt64(dataGridView1.Rows[1].Cells[i].Value));
+                mathPoints.Add(Convert.ToInt64(dataGridView1.Columns[i].HeaderCell.Value), Convert.ToInt64(dataGridView1.Rows[0].Cells[i].Value));
+            }
+            pane.XAxis.Title.Text = "Год";
+            pane.YAxis.Title.Text = "Средний балл";
+            pane.AddBar("Русский язык", rusPoints, Color.Blue);
+            pane.AddBar("Математика", mathPoints, Color.Black);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            zedGraphControl1.GraphPane.CurveList[0].Color = colorDialog1.Color;
+            zedGraphControl1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            zedGraphControl1.GraphPane.CurveList[0].Color = colorDialog1.Color;
+            zedGraphControl1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            zedGraphControl1.GraphPane.CurveList[1].Color = colorDialog1.Color;
+            zedGraphControl1.Refresh();
+        }
+    }
+}
